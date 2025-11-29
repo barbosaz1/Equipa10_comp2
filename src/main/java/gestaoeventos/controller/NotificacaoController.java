890290ec9b388203ec26cc78daf132b92db1e916
franchini06 <goncalo.franchini@gmail.com>
2025-11-29 22:13:46 +0000

    feat: create NotificacaoController for managing notifications

---
+++ src/main/java/gestaoeventos/controller/NotificacaoController.java
@@ -0,0 +1,11 @@
+package gestaoeventos.controller;
+
+import gestaoeventos.dto.NotificacaoDTO;
+import gestaoeventos.service.NotificacaoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class NotificacaoController {
+
+}
