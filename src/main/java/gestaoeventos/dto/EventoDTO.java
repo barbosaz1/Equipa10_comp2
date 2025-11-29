9cc4c01ffbcc496e081e39be6905c7fa4e2ddd49
FA507 <FA507@Tiago>
2025-11-29 21:39:49 +0000

    getter and setters da classe eventodto

--- src/main/java/gestaoeventos/dto/EventoDTO.java
+++ src/main/java/gestaoeventos/dto/EventoDTO.java
@@ -19,5 +19,105 @@ public class EventoDTO {
     private Integer criadorNumero;
     private Integer localId;
     private String motivoRemocao;
+    
+    public EventoDTO() {}
+
+    // getters e setters ...
+    
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDateTime dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDateTime getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDateTime dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public Integer getMaxParticipantes() {
+		return maxParticipantes;
+	}
+
+	public void setMaxParticipantes(Integer maxParticipantes) {
+		this.maxParticipantes = maxParticipantes;
+	}
+
+	public EstadoEvento getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoEvento estado) {
+		this.estado = estado;
+	}
+
+	public TipoEvento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEvento tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getAreaTematica() {
+		return areaTematica;
+	}
+
+	public void setAreaTematica(String areaTematica) {
+		this.areaTematica = areaTematica;
+	}
+
+	public Integer getCriadorNumero() {
+		return criadorNumero;
+	}
+
+	public void setCriadorNumero(Integer criadorNumero) {
+		this.criadorNumero = criadorNumero;
+	}
+
+	public Integer getLocalId() {
+		return localId;
+	}
+
+	public void setLocalId(Integer localId) {
+		this.localId = localId;
+	}
+
+	public String getMotivoRemocao() {
+		return motivoRemocao;
+	}
+
+	public void setMotivoRemocao(String motivoRemocao) {
+		this.motivoRemocao = motivoRemocao;
+	}
 
 }
