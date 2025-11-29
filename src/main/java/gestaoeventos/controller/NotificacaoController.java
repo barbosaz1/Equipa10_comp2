31aa9ceb95aaf4d95733a8d408d7fb43766f3a5d
franchini06 <goncalo.franchini@gmail.com>
2025-11-29 22:14:49 +0000

    feat: implement NotificacaoController for managing notifications

--- src/main/java/gestaoeventos/controller/NotificacaoController.java
+++ src/main/java/gestaoeventos/controller/NotificacaoController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/api/notificacoes")
 public class NotificacaoController {
 
+    private final NotificacaoService notificacaoService;
+
+    public NotificacaoController(NotificacaoService notificacaoService) {
+        this.notificacaoService = notificacaoService;
+    }
 }
