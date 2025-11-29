ae400494e5fcae807c4858aab8be71b23bfce850
FA507 <FA507@Tiago>
2025-11-29 21:36:13 +0000

    classe eventodto implementada com os seus respetivos atributos e packages

---
+++ src/main/java/gestaoeventos/dto/EeventoDTO.java
@@ -0,0 +1,23 @@
+package gestaoeventos.dto;
+
+import gestaoeventos.entity.EstadoEvento;
+import gestaoeventos.entity.TipoEvento;
+
+import java.time.LocalDateTime;
+
+public class EeventoDTO {
+	
+	private Integer id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataFim;
+    private Integer maxParticipantes;
+    private EstadoEvento estado;
+    private TipoEvento tipo;
+    private String areaTematica;
+    private Integer criadorNumero;
+    private Integer localId;
+    private String motivoRemocao;
+
+}
