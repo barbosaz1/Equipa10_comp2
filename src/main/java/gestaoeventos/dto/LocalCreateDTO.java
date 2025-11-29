b8578d7e6363d2aad9c308258110b190435445b1
p <p@DESKTOP-NN3A66F>
2025-11-29 21:30:02 +0000

    feat: add LocalCreateDTO for creating event locations

---
+++ src/main/java/gestaoeventos/dto/LocalCreateDTO.java
@@ -0,0 +1,11 @@
+package gestaoeventos.dto;
+
+public class LocalCreateDTO {
+	private String nome;
+    private String morada;
+    private Integer capacidade;
+    private String disponibilidadeHoraria;
+    private Boolean ativo;
+
+    public LocalCreateDTO() {}
+}
