bf212a519cdeb731d863432fe31634175ad41553
franchini06 <goncalo.franchini@gmail.com>
2025-11-29 22:16:46 +0000

    feat: create LogAuditoriaController for managing audit logs

---
+++ src/main/java/gestaoeventos/controller/LogAuditoriaController.java
@@ -0,0 +1,7 @@
+package gestaoeventos.controller;
+
+import gestaoeventos.dto.LogAuditoriaDTO;
+import gestaoeventos.service.LogAuditoriaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
