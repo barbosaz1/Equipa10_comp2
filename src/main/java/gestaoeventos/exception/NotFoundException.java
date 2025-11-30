dba6df186ec67d772975dafd9d000205b2fb929c
Rodrigo Barbosa <rb6544758@gmail.com>
2025-11-30 15:16:56 +0000

    refactor: add documentation comments to NotFoundException class

--- src/main/java/gestaoeventos/exception/NotFoundException.java
+++ src/main/java/gestaoeventos/exception/NotFoundException.java
@@ -1,9 +1,15 @@
 package gestaoeventos.exception;
 
+/**
+ * Excepção lançada quando um recurso não é encontrado.
+ */
 public class NotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Construtor com mensagem explicativa.
+     */
     public NotFoundException(String message) {
         super(message);
     }
