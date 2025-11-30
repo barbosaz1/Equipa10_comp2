4ea7a0e58b17edef22645dfabf3606b2eed9da42
Rodrigo Barbosa <rb6544758@gmail.com>
2025-11-30 15:16:21 +0000

    refactor: add documentation comments to PasswordConfig class

--- src/main/java/gestaoeventos/config/PasswordConfig.java
+++ src/main/java/gestaoeventos/config/PasswordConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordConfig {
 
+    // Bean que fornece um PasswordEncoder.
+    // Usamos BCrypt.
     @Bean
     public PasswordEncoder passwordEncoder() {
         // BCrypt default strength is 10
